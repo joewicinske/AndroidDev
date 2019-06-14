c6408c19ca5f103a028848a5b7e007057cfc54ff
joewicinske <50153898+joewicinske@users.noreply.github.com>
2019-06-13 21:11:09 -0700

    DBHelper
    Added DBHelper

---
+++ app/src/main/java/com/example/homework3/DBHelper.java
@@ -0,0 +1,82 @@
+package com.example.homework3;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    //breakfast, lunch, dinner, fasting, entry date, notes
+    public static final String DATABASE_NAME = "GlucoseDb.db";
+    public static final String GLUCOSE_COLUMN_BREAKFAST = "breakfast";
+    public static final String GLUCOSE_COLUMN_LUNCH = "lunch";
+    public static final String GLUCOSE_COLUMN_DINNER = "dinner";
+    public static final String GLUCOSE_COLUMN_FASTING = "fasting";
+    public static final String GLUCOSE_COLUMN_ENTRY_DATE = "entryDate";
+    public static final String GLUCOSE_COLUMN_ID = "id";
+    public static final String GLUCOSE_COLUMN_NOTES = "notes";
+
+    public DBHelper(Context context){
+        super(context, DATABASE_NAME, null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table glucose (id integer primary key, breakfast integer, lunch integer, dinner integer, fasting integer, entryDate text, notes text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS glucose");
+        onCreate(db);
+    }
+
+    public void insertGlucose(GlucoseData glucoseData){
+        ContentValues contentValues = new ContentValues();
+        SQLiteDatabase db = this.getWritableDatabase();
+        contentValues.put(GLUCOSE_COLUMN_BREAKFAST, glucoseData.getBreakfast());
+        contentValues.put(GLUCOSE_COLUMN_LUNCH, glucoseData.getLunch());
+        contentValues.put(GLUCOSE_COLUMN_NOTES, glucoseData.getNotes());
+        contentValues.put(GLUCOSE_COLUMN_DINNER, glucoseData.getDinner());
+        contentValues.put(GLUCOSE_COLUMN_FASTING, glucoseData.getFasting());
+        contentValues.put(GLUCOSE_COLUMN_ENTRY_DATE, glucoseData.getEntryDate());
+        db.insert("glucose", null, contentValues);
+    }
+
+    public int getNumRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return (int) DatabaseUtils.queryNumEntries(db, "glucose");
+    }
+
+    public void deleteGlucose(int id ){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete("glucose", "id = ?", new String[]{String.valueOf(id)});
+
+    }
+
+    public ArrayList<GlucoseData> getAllData(){
+        ArrayList<GlucoseData> data = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery("select * from glucose", null);
+        c.moveToFirst();
+        while (c.isAfterLast() == false){
+            GlucoseData glucoseData = new GlucoseData();
+            glucoseData.setLunch(c.getInt(c.getColumnIndex(GLUCOSE_COLUMN_LUNCH)));
+            glucoseData.setBreakfast(c.getInt(c.getColumnIndex(GLUCOSE_COLUMN_BREAKFAST)));
+            glucoseData.setDinner(c.getInt(c.getColumnIndex(GLUCOSE_COLUMN_DINNER)));
+            glucoseData.setId(c.getInt(c.getColumnIndex(GLUCOSE_COLUMN_ID)));
+            glucoseData.setFasting(c.getInt(c.getColumnIndex(GLUCOSE_COLUMN_FASTING)));
+            glucoseData.setNotes(c.getString(c.getColumnIndex(GLUCOSE_COLUMN_NOTES)));
+            glucoseData.setEntryDate(c.getString(c.getColumnIndex(GLUCOSE_COLUMN_ENTRY_DATE)));
+            data.add(glucoseData);
+            c.moveToNext();
+        }
+        return data;
+    }
+
+
+}
--- app/src/main/java/com/example/homework3/GlucoseData.java
+++ app/src/main/java/com/example/homework3/GlucoseData.java
@@ -7,6 +7,8 @@ import java.util.Locale;
 public class GlucoseData {
     String entryDate; //
     int fasting;
+    String notes = "";
+    int id;
 
     public GlucoseData(int fasting, int breakfast, int lunch, int dinner) {
         this.fasting = fasting;
@@ -19,7 +21,7 @@ public class GlucoseData {
     int dinner;
     int lunch;
     int average;
-    int id;
+
     boolean isNormal;
 
     public GlucoseData(){
@@ -95,5 +97,13 @@ public class GlucoseData {
                 new SimpleDateFormat(pattern, new Locale("en", "US"));
         String date = simpleDateFormat.format(myDate);
         return date;
+    }
+
+    public void setNotes(String notes){
+        this.notes = notes;
+    }
+
+    public String getNotes(){
+        return this.notes;
     }
 }
